5f1fa1152bf451636a1983f7f72a4987a6ec75e7
Renny Galindez <rfgalindez@gmail.com>
2021-05-16 13:30:59 -0500

    fixes

--- src/app.js
+++ src/app.js
@@ -27,14 +27,6 @@ server.use(cors(corsOptions));
 
 
 
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://central.vercel.app'); // update to match the domain you will make the request from
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   next();
-// });
-
 // rutas
 server.use(routes);
 server.use(errorHandler);
