81cec5f56bcea7775c0863d5e135ca8202104413
avila sebastian <avila.sebastian1490@gmail.com>
2021-05-16 13:31:00 -0300

    fixes #5

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const {
   getUsers,
+  getBookings,
 } = require('../controllers/users.js');
 const middlewareError = require('../middleware/middlewareError.js');
 
 router
   .route('/')
   .get(middlewareError(getUsers));
-
+router
+  .route('/bookings')
+  .get(middlewareError(getBookings));
 module.exports = router;
