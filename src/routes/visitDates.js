9fdc51d2faf11b6db3b8e936daadfde3d0e55363
avila sebastian <avila.sebastian1490@gmail.com>
2021-05-26 12:22:44 -0300

    endpoint bookings #42

--- src/routes/visitDates.js
+++ src/routes/visitDates.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {
   getAllMyBookings,
-} = require('../controllers/user.js');
+} = require('../controllers/booking.js');
 const middlewareError = require('../middleware/middlewareError.js');
 
-router.route('/:id')// id publicacion en construccion
+router.route('/:id')// id del usuario |id publicacion en construccion
   .get(middlewareError(getAllMyBookings));
 
 module.exports = router;
