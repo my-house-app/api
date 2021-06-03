32e7e9cd5146c66f8ee9ceb27f24a67ae59751a6
avila sebastian <avila.sebastian1490@gmail.com>
2021-06-03 13:51:57 -0300

    Los emails con numero null ahora es - . También se puede buscar post con tildes

--- src/utils.js
+++ src/utils.js
@@ -44,26 +44,26 @@ function buildEqual(number) {
 function buidlWhere(block) {
   // eslint-disable-next-line no-shadow
   const query = [];
-  if (block.post_name)    query.push({ post_name:    buildIlike(block.post_name)    });
-  if (block.city)         query.push({ city:         buildIlike(block.city)         });
+  if (block.post_name) query.push({ post_name: buildIlike(block.post_name) });
+  if (block.city) query.push({ city: buildIlike(block.city) });
   if (block.neighborhood) query.push({ neighborhood: buildIlike(block.neighborhood) });
-  query.push({ price:     buildMinMax(block.priceMin, block.priceMax) });
-  query.push({ m2:        buildMinMax(block.areaMin, block.areaMax)   });
-  query.push({ rooms:     buildMinMax(block.rooms, null)              });
-  query.push({ bathrooms: buildMinMax(block.bathrooms, null)          });
-  if (block.stratum)      query.push({ stratum:      buildEqual(block.stratum)     });
-  if (block.years)        query.push({ years:        buildEqual(block.years)       });
-  if (block.prop_type)    query.push({ prop_type:    buildIlike(block.prop_type)   });
-  if (block.pool)         query.push({ pool:         buildEqual(block.pool)        });
-  if (block.backyard)     query.push({ backyard:     buildEqual(block.backyard)    });
-  if (block.gym)          query.push({ gym:          buildEqual(block.gym)         });
-  if (block.bbq)          query.push({ bbq:          buildEqual(block.bbq)         });
-  if (block.parking_lot)  query.push({ parking_lot:  buildEqual(block.parking_lot) });
-  if (block.elevator)     query.push({ elevator:     buildEqual(block.elevator)    });
-  if (block.security)     query.push({ security:     buildEqual(block.security)    });
-  if (block.garden)       query.push({ garden:       buildEqual(block.garden)      });
-  if (block.status)       query.push({ status:       buildEqual(block.status)      });
-  if (block.active)       query.push({ active:       buildEqual(block.active)      });
+  query.push({ price: buildMinMax(block.priceMin, block.priceMax) });
+  query.push({ m2: buildMinMax(block.areaMin, block.areaMax) });
+  query.push({ rooms: buildMinMax(block.rooms, null) });
+  query.push({ bathrooms: buildMinMax(block.bathrooms, null) });
+  if (block.stratum) query.push({ stratum: buildEqual(block.stratum) });
+  if (block.years) query.push({ years: buildEqual(block.years) });
+  if (block.prop_type) query.push({ prop_type: buildIlike(block.prop_type) });
+  if (block.pool) query.push({ pool: buildEqual(block.pool) });
+  if (block.backyard) query.push({ backyard: buildEqual(block.backyard) });
+  if (block.gym) query.push({ gym: buildEqual(block.gym) });
+  if (block.bbq) query.push({ bbq: buildEqual(block.bbq) });
+  if (block.parking_lot) query.push({ parking_lot: buildEqual(block.parking_lot) });
+  if (block.elevator) query.push({ elevator: buildEqual(block.elevator) });
+  if (block.security) query.push({ security: buildEqual(block.security) });
+  if (block.garden) query.push({ garden: buildEqual(block.garden) });
+  if (block.status) query.push({ status: buildEqual(block.status) });
+  if (block.active) query.push({ active: buildEqual(block.active) });
   return { [Sequelize.Op.and]: query };
 }
 /**
@@ -74,15 +74,15 @@ function buidlWhere(block) {
 function buidlWhereUser(block) {
   // eslint-disable-next-line no-shadow
   const query = [];
-  if (block.email)         query.push({ email:        buildIlike(block.email) });
-  if (block.name)          query.push({ name:         buildIlike(block.name) });
-  if (block.city)          query.push({ city:         buildIlike(block.city) });
-  if (block.phone)         query.push({ phone:        buildEqual(block.phone) });// integer
-  if (block.photo)         query.push({ photo:        buildIlike(block.photo) });
-  if (block.password)      query.push({ password:     buildIlike(block.password) });
-  if (block.street_number) query.push({ street_number:buildIlike(block.street_number) });
-  if (block.zip_code)      query.push({ zip_code:     buildIlike(block.zip_code) });
-  if (block.type)          query.push({ type:         buildIlike(block.type) });
+  if (block.email) query.push({ email: buildIlike(block.email) });
+  if (block.name) query.push({ name: buildIlike(block.name) });
+  if (block.city) query.push({ city: buildIlike(block.city) });
+  if (block.phone) query.push({ phone: buildEqual(block.phone) });// integer
+  if (block.photo) query.push({ photo: buildIlike(block.photo) });
+  if (block.password) query.push({ password: buildIlike(block.password) });
+  if (block.street_number) query.push({ street_number: buildIlike(block.street_number) });
+  if (block.zip_code) query.push({ zip_code: buildIlike(block.zip_code) });
+  if (block.type) query.push({ type: buildIlike(block.type) });
   return { [Sequelize.Op.and]: query };
 }
 
@@ -117,6 +117,15 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/**
+ *
+ * @param {*} texto es un string
+ * @returns retorna un string sin tildes
+ */
+function changeDiacriticos(texto) {
+  return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
 module.exports = {
   buidlWhere,
   buildEqual,
@@ -127,4 +136,5 @@ module.exports = {
   isRegEx,
   buildFindByArray,
   getRandomInt,
+  changeDiacriticos,
 };
