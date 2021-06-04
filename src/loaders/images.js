766908eeba8b376f5cc254ede48e4d0e1394a518
maguilococo <56405247+maguilococo@users.noreply.github.com>
2021-06-04 10:07:47 -0300

    added fixed images

--- src/loaders/images.js
+++ src/loaders/images.js
@@ -5,7 +5,7 @@ const posts = require('./posts');
 const postsId = posts.map((p) => p.id);
 const images = rawData.map(({ imageLink }) => ({
   id: uuidv4(),
-  photo: [imageLink, 'https://metrocuadrado.blob.core.windows.net/inmuebles/2453-M2713849/2453-M2713849_7_h.jpg', 'https://metrocuadrado.blob.core.windows.net/inmuebles/2453-M2713849/2453-M2713849_12_h.jpg'],
+  photo: [imageLink, 'https://images.unsplash.com/photo-1584622650111-993a426fbf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80', 'https://images.unsplash.com/photo-1505692952047-1a78307da8f2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1158&q=80'],
   postId: postsId.pop(),
 }));
 
