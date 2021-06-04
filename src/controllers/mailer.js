584ecb5f472fd866d3612edca225f0e866eab177
pablo-chaves <76979597+pablo-chaves@users.noreply.github.com>
2021-06-04 12:11:29 -0500

    Update mailer.js

--- src/controllers/mailer.js
+++ src/controllers/mailer.js
@@ -183,7 +183,7 @@ async function sendBooking(req, res) {
                 display: block;
                 overflow: hidden;">
         <div style="display: flex;flex-direction: row;justify-content: left;">
-          <img style= "width: 15rem; height: 10rem;" src=${logo} alt='logo' />
+          <img style= "width: 20rem;" src=${logo} alt='logo' />
         </div>
         <hr />
 
